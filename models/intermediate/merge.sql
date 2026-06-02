0f3bac729c0fe8b03ab2361a216f5a0180db18a3
Diala Kikano <dkikano@gmail.com>
2026-06-02 07:23:43 +0000

    merge on user id

--- models/intermediate/merge.sql
+++ models/intermediate/merge.sql
@@ -1,12 +1,22 @@
 SELECT
-    u.*,
-    d.*,
-    t.*,
-    n.*
+    u.* EXCEPT(created_date),
+    u.created_date AS user_created_date,
+
+    d.* EXCEPT(user_id),
+
+    t.* EXCEPT(user_id, created_date),
+    t.created_date AS transaction_created_date,
+
+    n.* EXCEPT(user_id, created_date),
+    n.created_date AS notification_created_date
+
 FROM {{ ref('stg_neo_bank__users') }} AS u
+
 LEFT JOIN {{ ref('stg_neo_bank__devices') }} AS d
     USING (user_id)
+
 LEFT JOIN {{ ref('stg_neo_bank__transactions') }} AS t
     USING (user_id)
+
 LEFT JOIN {{ ref('stg_neo_bank__notifications') }} AS n
     USING (user_id)
