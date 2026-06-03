43bcda8fc0e8d947f49e6817e0c1b6e6006a44b9
Pierre Gorrity <pierre.gorrity@gmail.com>
2026-06-03 08:13:40 +0000

    ajout date_churn à users_full

--- models/intermediate/transactions_group_by_user.sql
+++ models/intermediate/transactions_group_by_user.sql
@@ -6,6 +6,8 @@ SUM(CASE WHEN direction = 'INBOUND' THEN amount_usd ELSE 0 END) AS total_amounts
 SUM(CASE WHEN direction = 'OUTBOUND' THEN 1 ELSE 0 END) AS nb_transactions_out,
 SUM(CASE WHEN direction = 'INBOUND' THEN 1 ELSE 0 END) AS nb_transactions_in,
 MIN(created_date) as date_first_transaction,
-MAX(created_date) as date_last_transaction
+MAX(created_date) as date_last_transaction,
+--CAST(DATETIME_ADD(DATETIME(MAX(created_date)), INTERVAL 3 MONTH) AS TIMESTAMP) AS date_churn
+EXTRACT(DATE FROM DATETIME_ADD(DATETIME(MAX(created_date)), INTERVAL 3 MONTH)) AS date_churn
 FROM {{ ref('stg_neo_bank__transactions') }} AS notif
 GROUP BY user_id
