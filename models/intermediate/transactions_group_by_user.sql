8f1c47158fb2694129659a606457e287586c750c
Pierre Gorrity <pierre.gorrity@gmail.com>
2026-06-03 10:33:53 +0000

    Ajout de 2 colonnes à users_full :  is_globtrotteur qui vaut 1 si le user a payé dans 4 devises différents, 0 sinon
    nb_devise : le nombre de devises utilisées

--- models/intermediate/transactions_group_by_user.sql
+++ models/intermediate/transactions_group_by_user.sql
@@ -5,6 +5,8 @@ SUM(CASE WHEN direction = 'OUTBOUND' THEN amount_usd ELSE 0 END) AS total_amount
 SUM(CASE WHEN direction = 'INBOUND' THEN amount_usd ELSE 0 END) AS total_amounts_transactions_in,
 SUM(CASE WHEN direction = 'OUTBOUND' THEN 1 ELSE 0 END) AS nb_transactions_out,
 SUM(CASE WHEN direction = 'INBOUND' THEN 1 ELSE 0 END) AS nb_transactions_in,
+COUNT(DISTINCT transactions_currency) as nb_devises,
+CASE WHEN COUNT(DISTINCT transactions_currency) > 4 THEN 1 ELSE 0 END AS is_globtrotter,
 MIN(created_date) as date_first_transaction,
 MAX(created_date) as date_last_transaction,
 --CAST(DATETIME_ADD(DATETIME(MAX(created_date)), INTERVAL 3 MONTH) AS TIMESTAMP) AS date_churn
