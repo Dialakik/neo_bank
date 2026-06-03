ddc779b95cb96539a02f86c841c8c82d1c8f7836
Pierre Gorrity <pierre.gorrity@gmail.com>
2026-06-03 07:38:30 +0000

    ajout total_amounts_transactions_out et total_amounts_transactions_in

--- models/intermediate/transactions_group_by_user.sql
+++ models/intermediate/transactions_group_by_user.sql
@@ -1,6 +1,8 @@
 SELECT
 user_id,
 count(*) as nb_transactions,
+SUM(CASE WHEN direction = 'OUTBOUND' THEN amount_usd ELSE 0 END) AS total_amounts_transactions_out,
+SUM(CASE WHEN direction = 'INBOUND' THEN amount_usd ELSE 0 END) AS total_amounts_transactions_in,
 SUM(CASE WHEN direction = 'OUTBOUND' THEN 1 ELSE 0 END) AS nb_transactions_out,
 SUM(CASE WHEN direction = 'INBOUND' THEN 1 ELSE 0 END) AS nb_transactions_in,
 MIN(created_date) as date_first_transaction,
