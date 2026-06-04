a0613fb7f8739f8dfe47b3f405e6acd42b3e9348
Pierre Gorrity <pierre.gorrity@gmail.com>
2026-06-04 08:40:12 +0000

    ajout colonne persona

--- models/mart/users_full.sql
+++ models/mart/users_full.sql
@@ -6,7 +6,13 @@ SELECT
 
     notifications.* EXCEPT(user_id),
 
-    devices.* EXCEPT(user_id)
+    devices.* EXCEPT(user_id),
+    CASE 
+        WHEN (users.birth_year > 30 and users.birth_year <= 40) and transactions.is_globtrotter = 1 THEN "30s GLOBE TROTTEUR" 
+        WHEN users.user_settings_crypto_unlocked = 1 THEN "CRYPTO LOVER" 
+        WHEN (users.birth_year > 15 and users.birth_year <= 25) and (total_amounts_in - total_amounts_out < 500) THEN "Jeune en galère" 
+        WHEN total_amounts_in - total_amounts_out > 2000 THEN "EPARGNANT"
+        ELSE "other people less interesting" END as persona
 
 FROM {{ ref('stg_neo_bank__users') }} AS users
 
