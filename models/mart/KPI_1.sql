de33be343276c7e6c700aed56e08be4cb6f0ba2a
Pierre Gorrity <pierre.gorrity@gmail.com>
2026-06-08 14:01:44 +0000

    ajout colonne taux_retention à tables KPI

--- models/mart/KPI_1.sql
+++ models/mart/KPI_1.sql
@@ -4,5 +4,6 @@ SELECT
   ROUND(COUNTIF(plan != 'STANDARD') / COUNT(*), 3)             AS pct_premium,
   ROUND(AVG(total_amounts_out), 0)                              AS depense_moy_par_user,
   ROUND(APPROX_QUANTILES(total_amounts_out, 100)[OFFSET(50)], 0) AS depense_mediane,
-  ROUND(AVG(nb_transactions), 1)                                AS nb_transac_moyen
+  ROUND(AVG(nb_transactions), 1)                                AS nb_transac_moyen,
+  ROUND(COUNTIF(date_churn > '2019-05-16')/ COUNT(*),1) AS taux_retention
 FROM `neobankprojectlewagon.dbt_lpignataro.users_full`
